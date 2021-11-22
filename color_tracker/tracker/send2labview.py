953e7439b61b2ca7572daf97a2f2478d0d92a6c1
postdoc <im_caimingxue@163.com>
2021-11-22 11:52:25 +0800

    update

--- color_tracker/tracker/send2labview.py
+++ color_tracker/tracker/send2labview.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-  dcdfr c                       fff# @Email : im_caimingxue@163.com
+# @Email : im_caimingxue@163.com
 # @github : https://github.com/caimingxue/magnetic-robot-simulation
 # @notice ：
 from color_tracker.utils.communication import TCP_Communication
@@ -12,7 +12,7 @@ def senddata2labview(x_pixel, y_pixel):
 
     x_world, y_world = image2world(x_pixel, y_pixel)
 
-      mag_frequency = 2.0
+    mag_frequency = 2.0
     mag_direction = 90.
     mag_pitch = 90.
     mag_conical = 45.
@@ -30,3 +30,11 @@ def senddata2labview(x_pixel, y_pixel):
 
 def image2world(x_pixel, y_pixel):
     pass
+
+# void mapToWorld(unsigned int mx, unsigned int my, double& wx, double& wy, nav_messages::OccupancyGrid& map)
+# {//+0.5 为了去除边界，防止计算到地图外面，(这里map 是指像素坐标系，单位为int，
+# //world 是指/map世界坐标系)
+# 	wx = map.info.origin.position.x + (mx + 0.5) * map.info.resolution;
+# 	wy = map.info.origin.position.y + (my + 0.5) * map.info.resolution;
+#
+# }
